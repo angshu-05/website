5eb0e73a90b75f459d04b03660117313b9258679
Angshuman Das Adhikary <angshumandasadhikary@gmail.com>
2025-06-23 20:37:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 // typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Android App Developer'],
+    strings: ['Android App Developer','UI/UX Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
